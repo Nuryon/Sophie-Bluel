3610b5eb91182805a97bd6d5d324c9a2b343c492
Nuryon <dutaillykevin.pro@gmail.com>
2024-05-23 03:47:19 +0200

    modal second part

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/script.js
@@ -57,7 +57,6 @@ const displayWorks = (categoryId) => {
   console.log(categoryId);
   const buttonFilter = document.getElementsByClassName("button-filter");
 
-  // Update button classes based on selected category
   for (let i = 0; i < buttonFilter.length; i++) {
     if (i === categoryId) {
       buttonFilter[i].className = "btn-filtre-active button-filter";
@@ -66,11 +65,9 @@ const displayWorks = (categoryId) => {
     }
   }
 
-  // Clear the main gallery
   const gallery = document.querySelector(".gallery");
   gallery.innerHTML = "";
 
-  // Filter works based on category
   let worksList = [];
   if (categoryId === 0) {
     worksList = works;
@@ -78,7 +75,6 @@ const displayWorks = (categoryId) => {
     worksList = works.filter((work) => work.categoryId === categoryId);
   }
 
-  // Populate the main gallery with images
   worksList.forEach((work) => {
     const imgElement = document.createElement("img");
     imgElement.src = work.imageUrl;
@@ -99,12 +95,13 @@ const generateModalFirstContent = () => {
 
   const title = document.createElement("h2");
   title.innerHTML = "Galerie photo";
-
+  title.className = "modal-maintitle";
   const buttonBar = document.createElement("div");
   buttonBar.className = "button-bar";
 
   const button = document.createElement("button");
   button.innerHTML = "Ajouter une photo";
+  button.classList.add("button-modal");
   button.addEventListener("click", generateModalSecondContent);
 
   modal.appendChild(title);
@@ -143,11 +140,15 @@ const generateModalFirstContent = () => {
   modal.appendChild(xMark);
   modal.appendChild(buttonBar);
 };
+
 const generateModalSecondContent = () => {
   const modal = document.querySelector(".modal");
   modal.innerHTML = "";
+
   const title = document.createElement("h2");
   title.innerHTML = "Ajout photo";
+  title.classList.add("modal-maintitle");
+
   const xMark = document.createElement("i");
   xMark.className = "fa-solid fa-xmark xMark";
   xMark.addEventListener("click", () => {
@@ -156,16 +157,92 @@ const generateModalSecondContent = () => {
       modalContent.remove();
     }
   });
+
   const buttonBar = document.createElement("div");
   buttonBar.className = "button-bar";
+
   const button = document.createElement("button");
   button.innerHTML = "Valider";
+  button.classList.add("button-modal");
+
+  const pictureDiv = document.createElement("div");
+  pictureDiv.className = "picture-div";
+
+  const iconPicture = document.createElement("i");
+  iconPicture.className = "fa-regular fa-image fa-5x ";
+
+  const buttonModalSeconContent = document.createElement("button");
+  const infoElement = document.createElement("p");
+  infoElement.classList.add("info-element");
+  infoElement.innerHTML = "jpg, png : 4mo max";
+
+  const secondTitle = document.createElement("h2");
+  secondTitle.innerHTML = "Titre";
+  secondTitle.classList.add("modal-subtitle");
+
+  const firstInput = document.createElement("input");
+  firstInput.className = "modal-input";
+
+  const thirdTitle = document.createElement("h2");
+  thirdTitle.innerHTML = "Catégorie";
+  thirdTitle.classList.add("modal-second-subtitle");
+
+  const secondInput = document.createElement("input");
+  secondInput.className = "modal-second-input";
+
+  const dropdownIcon = document.createElement("i");
+  dropdownIcon.className = "fa-solid fa-chevron-down";
+
+  const inputWrapper = document.createElement("div");
+  inputWrapper.className = "input-container";
+
+  inputWrapper.appendChild(secondInput);
+  inputWrapper.appendChild(dropdownIcon);
+
+  const dropdownMenu = document.createElement("div");
+  dropdownMenu.className = "dropdown-content";
+  const link1 = document.createElement("a");
+  link1.href = "#";
+  link1.textContent = "Objets";
+  const link2 = document.createElement("a");
+  link2.href = "#";
+  link2.textContent = "Appartements";
+  const link3 = document.createElement("a");
+  link3.href = "#";
+  link3.textContent = "Hotels & restaurants";
+
+  dropdownMenu.appendChild(link1);
+  dropdownMenu.appendChild(link2);
+  dropdownMenu.appendChild(link3);
+
+  inputWrapper.appendChild(dropdownMenu);
+
+  dropdownIcon.addEventListener("click", () => {
+    dropdownMenu.classList.toggle("show");
+  });
+
+  window.addEventListener("click", (e) => {
+    if (!inputWrapper.contains(e.target)) {
+      dropdownMenu.classList.remove("show");
+    }
+  });
+
+  buttonModalSeconContent.classList.add("button-modal-second");
+  buttonModalSeconContent.innerHTML = "+ Ajouter photo";
+  pictureDiv.appendChild(iconPicture);
+  pictureDiv.appendChild(buttonModalSeconContent);
+  pictureDiv.appendChild(infoElement);
   buttonBar.appendChild(button);
   button.addEventListener("click", generateModalFirstContent);
+
   modal.appendChild(title);
   modal.appendChild(xMark);
-
+  modal.appendChild(pictureDiv);
   modal.appendChild(buttonBar);
+  modal.appendChild(secondTitle);
+  modal.appendChild(firstInput);
+  modal.appendChild(thirdTitle);
+  modal.appendChild(inputWrapper);
 };
 
 const generateModal = () => {
